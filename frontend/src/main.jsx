3a79901a445b52fc37b4bfa9e6088e18d1106bf6
67160230-byte <67160230@go.buu.ac.th>
2026-06-20 07:14:17 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,20 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import "./theme.css";
+import { EscoProvider } from "./context/EscoContext";
+import Navbar from "./components/Navbar";
 import InputPage from "./pages/InputPage";
 import EcoScorePage from "./pages/EcoScorePage";
 import CodeIssuePage from "./pages/CodeIssuePage";
 import RecommendationPage from "./pages/RecommendationPage";
 
+function App() {
+  return (
+    <EscoProvider>
+      <Navbar />
+      <div className="app-shell">
+        <Routes>
+          <Route path="/" element={<InputPage />} />
+          <Route path="/eco-score" element={<EcoScorePage />} />
+          <Route path="/issues" element={<CodeIssuePage />} />
+          <Route path="/recommendations" element={<RecommendationPage />} />
+        </Routes>
+      </div>
+    </EscoProvider>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<InputPage />} />
-        <Route path="/eco-score" element={<EcoScorePage />} />
-        <Route path="/issues" element={<CodeIssuePage />} />
-        <Route path="/recommendations" element={<RecommendationPage />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
